a3d0bda52624994cd1a43ff8bdad2d7d2255ce9b
ayanared <ayanared.gmail.com>
2018-01-04 12:20:42 -0500

    schemas

---
+++ db/schema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const FavLayoutSchema = new Schema({
+    name: String,
+    img: String,
+    num_of_cards: Number,
+    description: String
+}, {usePushEach: true})
+
+const ReadingSchema = new Schema({
+    query_nickname: String,
+    question: String,
+    cards_index: [Number],
+    date: String
+}, {usePushEach: true})
+
+
+const UserSchema = new Schema({
+    name: {type: String, required: true},
+    picture_link: String,
+    favLayout: [FavLayoutSchema],
+    reading: [ReadingSchema]
+
+}, {usePushEach: true})
+
+module.exports = {
+    UserSchema,
+    ReadingSchema,
+    FavLayoutSchema
+}
