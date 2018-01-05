e1cc876507a5bb26bd953c412f2c251f27abc046
ayanared <ayanared.gmail.com>
2018-01-05 14:58:38 -0500

    added on click events to cards

--- public/javascripts/get_new_reading.js
+++ public/javascripts/get_new_reading.js
@@ -1,3 +1,11 @@
-$(()=>{
+$(() => {
     console.log("get a reading!")
+    const cards = createCards()
+    for (let i = 0; i < 3; i++) {
+        $(`#current_card${i}_img`).on("click", () => {
+            const random_card = Math.floor(Math.random() * cards.length);
+
+            console.log(`you clicked ${i}, the random number is: ${random_card}`)
+        })
+    }
 })
