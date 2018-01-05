86b75eb7e64f7287be6e4e544dffbedac978275d
ayanared <ayanared.gmail.com>
2018-01-05 12:03:55 -0500

    added paragraph descriptions

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,14 +2,19 @@ $(()=>{
     console.log("here")
     //populating past cards pulled
     
-    const val0 = $('#card0val').val()
+    const val0 = $('#past_val_card0').val()
     $('#card0img_past').attr('src', `/${cards[val0].cardImage}`)
     console.log(val0)
-    const val1 = $('#card1val').val()
+    const val1 = $('#past_val_card1').val()
     $('#card1img_past').attr('src', `/${cards[val1].cardImage}`)
     console.log(val1)
-    const val2 = $('#card2val').val()
+    const val2 = $('#past_val_card2').val()
     $('#card2img_past').attr('src', `/${cards[val2].cardImage}`)
     console.log(val2)
+
+    //populating descriptions for each card
+    $('#past_description_card0').text(cards[val0].cardDescription)
+    $('#past_description_card1').text(cards[val1].cardDescription)
+    $('#past_description_card2').text(cards[val2].cardDescription)
     
 })
