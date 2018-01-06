0d01c935298cff49a486049ac052540ccdbf0cf1
ayanared <ayanared.gmail.com>
2018-01-05 22:37:39 -0500

    added date to new readings

--- public/javascripts/get_new_reading.js
+++ public/javascripts/get_new_reading.js
@@ -1,4 +1,16 @@
+const date = new Date();
+const month_number = date.getMonth();
+const day_of_month = date.getDate();
+const year = date.getFullYear();
+
+const month = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
 $(() => {
+    const full_date = month[month_number] +`, ${day_of_month}, ${year}`
+    $('#date').val(full_date)
     console.log("get a reading!")
     const cards = createCards()
     for (let i = 0; i < 3; i++) {
@@ -11,7 +23,7 @@ $(() => {
             $(`#card_index_${i}`).val(pulled_card.number)
             //change picture
             $(`#current_card${i}_img`).attr('src', `/${pulled_card.cardImage}`)
-            $(`#current_header_card${i}`).text(`Card ${i+1}`)
+            $(`#current_header_card${i}`).text(`Card ${i + 1}`)
             $(`#current_description_card${i}`).text(`${pulled_card.cardDescription}`)
             console.log(`you clicked ${i}, the random number is: ${pulled_card.number} which is ${pulled_card.name}`)
         })
